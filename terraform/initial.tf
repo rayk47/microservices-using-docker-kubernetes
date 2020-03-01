4bfedc74f3869cb9cdc6a43f2d0170638763e13a
rayk47 <raymond.kelly43@mail.dcu.ie>
2020-02-29 22:33:56 -0800

    Added initial terraform work

---
+++ terraform/initial.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip}"
+  }
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
